fb8c82ec8fe0b4d542567d590be359146706fee0
Jaime-Poveda2007 <jaime.povedam@uniagustiniana.edu.co>
2026-04-23 22:07:48 +0000

    agregamos capacitor

---
+++ android/app/src/main/java/com/poetapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.poetapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.poetapp.app',
+  appName: 'PoetApp',
+  webDir: 'dist'
+};
+
+export default config;
