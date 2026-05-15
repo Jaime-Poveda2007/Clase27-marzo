080ace18b0a3d34255c4b3685cd2641b86f7b092
Jaime-Poveda2007 <jaime.povedam@uniagustiniana.edu.co>
2026-05-15 19:32:45 +0000

    archivo conexion a firebase

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+apiKey: "AIzaSyA9L0mZ_sXRUAtBj1f01LHNuM8RC5_hoMI",
+authDomain:  "individualapi-1db1b.firebaseapp.com",
+projectId: "individualapi-1db1b",
+storageBucket: "individualapi-1db1b.firebasestorage.app",
+messagingSenderId: "967070280743",
+appId: "1:967070280743:web:eb02f46dc6de081b7b3202",
+measurementId: "G-PMCHYQ0LRY"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+export { auth, db };
